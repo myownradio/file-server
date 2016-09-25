d0c848d64c99c95612b5e1fac386a5934188c32d
woobinda <woobinda@voliacable.com>
2016-09-25 12:05:46 +0300

    add argparse

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*- 
-import os, hashlib
+import os, hashlib, argparse
 from flask import Flask, request, Response, send_from_directory, jsonify, abort
 from werkzeug.utils import secure_filename
 
 
-port = 7000							#параметры получаемые с консоли
-hash_algo = 'sha1'
-content_dir = 'UPLOAD1'
+parser = argparse.ArgumentParser()			#настройка приема аргументов с командной строки
+parser.add_argument("--port", default='7000', type=int, dest='port', help='Port to listen'),
+parser.add_argument("--hash-algo", default='ash1', type=str, dest='hash_algo', help='Hashing algorithm to use'),
+parser.add_argument("--content-dir", type=str, default='UPLOAD', dest='content_dir', help='Enable folder to upload'),
+args = parser.parse_args()
+
+port = args.port							#параметры получаемые с консоли
+hash_algo = args.hash_algo
+content_dir = args.content_dir
+
 
 BASE_DIR = os.path.abspath('.')
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'UPLOADS', content_dir)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024			#16 mb
 
 
 def sha1(file):								#функция возвращает sha1 хеш, загружаемого файла
@@ -31,7 +39,7 @@ def upload_file():
 			path = app.config['UPLOAD_FOLDER']
 			if os.path.exists(path) == False:
 				os.mkdir(app.config['UPLOAD_FOLDER'])
-			filename = secure_filename(file.filename)									#защита от инъекций в имени загружаемого файла
+			filename = secure_filename(file.filename)					#защита от инъекций в имени загружаемого файла
 			path_to_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)						
 			file.save(path_to_file)
 			if hash_algo == 'sha1':
@@ -55,13 +63,12 @@ def get_file_folder(filename_hash):						#фун-я определяет в ка
 	for obj in os.listdir(BASE_DIR + '/UPLOADS'):
 		path = os.path.join(BASE_DIR, 'UPLOADS', obj)
 		if os.path.isdir(path):
-			path_to_folder = path
-			if filename_hash in os.listdir(path_to_folder):
-				return(path_to_folder)
+			if filename_hash in os.listdir(path):
+				return(path)
 
 
 @app.route('/file/<filename_hash>', methods=['GET', 'DELETE'])
-def file_detail(filename_hash):
+def detail_file(filename_hash):
 	if len(filename_hash) == 40:
 		try:
 			FOLDER = get_file_folder(filename_hash)
@@ -74,8 +81,7 @@ def file_detail(filename_hash):
 
 		if request.method == 'DELETE':
 			file = os.path.join(FOLDER, filename_hash)
-			os.remove(file)										#удаляем указаный файл
-			return 'remove %s' %filename_hash
+			os.remove(file)											#удаляем указаный файл
 	else:
 		abort(404)
 
@@ -83,9 +89,9 @@ def file_detail(filename_hash):
 @app.route('/status', methods=['GET'])
 def get_status():
 	disc = os.statvfs(BASE_DIR)
-	free_space = disc.f_bsize * disc.f_bavail / 1024 / 1024					#функция возвращает оставшееся свободное место на диске в мб
+	free_space = disc.f_bsize * disc.f_bavail / 1024 / 1024			#функция возвращает оставшееся свободное место на диске в mb
 	return jsonify({'free space':free_space, 'units':'mb'})
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=port, threaded=True)						#host='0.0.0.0'
+   	app.run(host='localhost', port=args.port, threaded=True)		#host='0.0.0.0'; threaded = True - рекамендация по запуску приложений без настроенного WSGI
